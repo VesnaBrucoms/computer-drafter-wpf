913c17bd419091f2280274e349f7f5c4314437ce
VesnaBrucoms <etstevens60@gmail.com>
2015-08-09 16:07:55 +0100

    Begun setting up MVVM pattern, and implemeting ini settings.

--- Computer Drafter/Views/MainWindow.xaml.cs
+++ Computer Drafter/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Computer_Drafter
+namespace Computer_Drafter.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
