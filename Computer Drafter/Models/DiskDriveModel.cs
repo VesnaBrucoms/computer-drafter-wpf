1465ba6781115ac534aab77dad7648181c095b12
VesnaBrucoms <etstevens60@gmail.com>
2015-08-16 19:03:11 +0100

    Begun adding disc drive.

---
+++ Computer Drafter/Models/DiskDriveModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer_Drafter.Models
+{
+    class DiscDriveModel : PartModel, IMultiPartModel, INotifyPropertyChanged
+    {
+        private int number;
+        private List<string> types;
+        private string selType;
+
+        #region Properties
+        public DiscDriveModel GetInstance
+        {
+            get { return this; }
+        }
+
+        public int PartNumber
+        {
+            get { return number; }
+            set
+            {
+                number = value;
+                OnPropertyChanged("PartNumber");
+                OnPropertyChanged("PartNumberString");
+            }
+        }
+
+        public string PartNumberString
+        {
+            get { return "Disk drive " + number; }
+        }
+
+        public List<string> GetTypes
+        {
+            get { return types; }
+        }
+
+        public string SelectedType
+        {
+            get { return selType; }
+            set { selType = value; }
+        }
+        #endregion
+
+        public DiscDriveModel()
+        {
+            types = new List<string>();
+            types.Add("CD-ROM");
+            types.Add("CD-R");
+            types.Add("CD-RW");
+            types.Add("DVD-ROM");
+            types.Add("DVD-R");
+            types.Add("DVD-RW");
+            types.Add("BD-ROM");
+            types.Add("BD-R");
+            types.Add("BD-RE");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+                Console.WriteLine(propertyName);
+            }
+        }
+    }
+}
---
+++ Computer Drafter/ViewModels/DiskDriveViewModel.cs
@@ -0,0 +1,86 @@
+﻿using Computer_Drafter.Models;
+using Computer_Drafter.ViewModels.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Computer_Drafter.ViewModels
+{
+    class DiscDriveViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<DiscDriveModel> discDrives;
+
+        #region ModelProperties
+        public ObservableCollection<DiscDriveModel> GetDiscDrives
+        {
+            get { return discDrives; }
+        }
+        #endregion
+
+        #region CommandProperties
+        public ICommand AddDiscDriveCommand
+        {
+            get { return new DelegateCommand(AddDiscDrive, CanAddDiscDrive); }
+        }
+
+        public ICommand RemoveDiscDriveCommand
+        {
+            get { return new DelegateCommand(RemoveDiscDrive, CanRemoveDiscDrive); }
+        }
+        #endregion
+
+        public DiscDriveViewModel()
+        {
+            discDrives = new ObservableCollection<DiscDriveModel>();
+        }
+
+        #region Commands
+        private void AddDiscDrive(object parameter)
+        {
+            discDrives.Add(new DiscDriveModel());
+            discDrives[discDrives.Count - 1].PartNumber = discDrives.Count;
+        }
+
+        private bool CanAddDiscDrive()
+        {
+            return true;
+        }
+
+        private void RemoveDiscDrive(object parameter)
+        {
+            DiscDriveModel model = (DiscDriveModel)parameter;
+            discDrives.Remove(model);
+
+            int count = 0;
+            foreach (DiscDriveModel drive in discDrives)
+            {
+                if (drive.PartNumber - 1 != count)
+                    drive.PartNumber = count + 1;
+
+                count++;
+            }
+        }
+
+        private bool CanRemoveDiscDrive()
+        {
+            return true;
+        }
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+                Console.WriteLine(propertyName);
+            }
+        }
+    }
+}
