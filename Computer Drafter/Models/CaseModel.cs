4d0cfdd92b4b511085e942542f802096da921ebb
VesnaBrucoms <etstevens60@gmail.com>
2015-08-19 20:33:03 +0100

    Finished case view.

---
+++ Computer Drafter/Models/CaseModel.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer_Drafter.Models
+{
+    class CaseModel : PartModel
+    {
+        private List<string> forms;
+        private string selForm;
+        private List<string> bayNumbers;
+        private string selFiveBays;
+        private string selThreeBays;
+        private string selTwoBays;
+        private bool acceptsItx;
+        private bool acceptsMatx;
+        private bool acceptsAtx;
+        private bool acceptsEatx;
+        private bool hasUsbThree;
+        private bool hasPsu;
+        private bool hasWindow;
+        private bool isToolless;
+
+        #region Properties
+        public List<string> GetForms
+        {
+            get { return forms; }
+        }
+
+        public string SelectedForm
+        {
+            get { return selForm; }
+            set { selForm = value; }
+        }
+
+        public List<string> GetBayNumbers
+        {
+            get { return bayNumbers; }
+        }
+
+        public string SelectedFiveBays
+        {
+            get { return selFiveBays; }
+            set { selFiveBays = value; }
+        }
+
+        public string SelectedThreeBays
+        {
+            get { return selThreeBays; }
+            set { selThreeBays = value; }
+        }
+
+        public string SelectedTwoBays
+        {
+            get { return selTwoBays; }
+            set { selTwoBays = value; }
+        }
+
+        public bool AcceptsItx
+        {
+            get { return acceptsItx; }
+            set { acceptsItx = value; }
+        }
+
+        public bool AcceptsMatx
+        {
+            get { return acceptsMatx; }
+            set { acceptsMatx = value; }
+        }
+
+        public bool AcceptsAtx
+        {
+            get { return acceptsAtx; }
+            set { acceptsAtx = value; }
+        }
+
+        public bool AcceptsEatx
+        {
+            get { return acceptsEatx; }
+            set { acceptsEatx = value; }
+        }
+
+        public bool HasUsbThree
+        {
+            get { return hasUsbThree; }
+            set { hasUsbThree = value; }
+        }
+
+        public bool HasPsu
+        {
+            get { return hasPsu; }
+            set { hasPsu = value; }
+        }
+
+        public bool HasWindow
+        {
+            get { return hasWindow; }
+            set { hasWindow = value; }
+        }
+
+        public bool IsToolless
+        {
+            get { return isToolless; }
+            set { isToolless = value; }
+        }
+        #endregion
+
+        public CaseModel()
+        {
+            forms = new List<string>();
+            forms.Add("Mini-ITX");
+            forms.Add("Micro-ATX");
+            forms.Add("ATX");
+            forms.Add("E-ATX");
+
+            bayNumbers = new List<string>();
+            bayNumbers.Add("1");
+            bayNumbers.Add("2");
+            bayNumbers.Add("3");
+            bayNumbers.Add("4");
+            bayNumbers.Add("5");
+            bayNumbers.Add("6");
+            bayNumbers.Add("7");
+            bayNumbers.Add("8");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+                Console.WriteLine(propertyName);
+            }
+        }
+    }
+}
---
+++ Computer Drafter/ViewModels/CaseViewModel.cs
@@ -0,0 +1,190 @@
+﻿using Computer_Drafter.Models;
+using Computer_Drafter.ViewModels.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer_Drafter.ViewModels
+{
+    class CaseViewModel : INotifyPropertyChanged
+    {
+        private CaseModel caseModel;
+
+        #region ModelProperties
+        public string CaseName
+        {
+            get { return caseModel.Name; }
+            set
+            {
+                caseModel.Name = value;
+                OnPropertyChanged("CaseName");
+            }
+        }
+
+        public decimal CasePrice
+        {
+            get { return caseModel.Price; }
+            set
+            {
+                caseModel.Price = value;
+                OnPropertyChanged("CasePrice");
+            }
+        }
+
+        public List<string> GetCaseFormsList
+        {
+            get { return caseModel.GetForms; }
+        }
+
+        public string SetCaseForm
+        {
+            get { return caseModel.SelectedForm; }
+            set
+            {
+                caseModel.SelectedForm = value;
+                OnPropertyChanged("SetCaseForm");
+            }
+        }
+
+        public List<string> GetCaseBayNumbersList
+        {
+            get { return caseModel.GetBayNumbers; }
+        }
+
+        public string SetCaseFiveBays
+        {
+            get { return caseModel.SelectedFiveBays; }
+            set
+            {
+                caseModel.SelectedFiveBays = value;
+                OnPropertyChanged("SetCaseFiveBays");
+            }
+        }
+
+        public string SetCaseThreeBays
+        {
+            get { return caseModel.SelectedThreeBays; }
+            set
+            {
+                caseModel.SelectedThreeBays = value;
+                OnPropertyChanged("SetCaseThreeBays");
+            }
+        }
+
+        public string SetCaseTwoBays
+        {
+            get { return caseModel.SelectedTwoBays; }
+            set
+            {
+                caseModel.SelectedTwoBays = value;
+                OnPropertyChanged("SetCaseTwoBays");
+            }
+        }
+
+        public bool CaseAcceptsItx
+        {
+            get { return caseModel.AcceptsItx; }
+            set
+            {
+                caseModel.AcceptsItx = value;
+                OnPropertyChanged("CaseAcceptsItx");
+            }
+        }
+
+        public bool CaseAcceptsMatx
+        {
+            get { return caseModel.AcceptsMatx; }
+            set
+            {
+                caseModel.AcceptsMatx = value;
+                OnPropertyChanged("CaseAcceptsMatx");
+            }
+        }
+
+        public bool CaseAcceptsAtx
+        {
+            get { return caseModel.AcceptsAtx; }
+            set
+            {
+                caseModel.AcceptsAtx = value;
+                OnPropertyChanged("CaseAcceptsAtx");
+            }
+        }
+
+        public bool CaseAcceptsEatx
+        {
+            get { return caseModel.AcceptsEatx; }
+            set
+            {
+                caseModel.AcceptsEatx = value;
+                OnPropertyChanged("CaseAcceptsEatx");
+            }
+        }
+
+        public bool CaseHasUsbThree
+        {
+            get { return caseModel.HasUsbThree; }
+            set
+            {
+                caseModel.HasUsbThree = value;
+                OnPropertyChanged("CaseHasUsbThree");
+            }
+        }
+
+        public bool CaseHasPsu
+        {
+            get { return caseModel.HasPsu; }
+            set
+            {
+                caseModel.HasPsu = value;
+                OnPropertyChanged("CaseHasPsu");
+            }
+        }
+
+        public bool CaseHasWindow
+        {
+            get { return caseModel.HasWindow; }
+            set
+            {
+                caseModel.HasWindow = value;
+                OnPropertyChanged("CaseHasWindow");
+            }
+        }
+
+        public bool CaseIsToolless
+        {
+            get { return caseModel.IsToolless; }
+            set
+            {
+                caseModel.IsToolless = value;
+                OnPropertyChanged("CaseIsToolless");
+            }
+        }
+        #endregion
+
+        #region CommandProperties
+        #endregion
+
+        public CaseViewModel()
+        {
+            caseModel = new CaseModel();
+        }
+
+        #region Commands
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+                Console.WriteLine(propertyName);
+            }
+        }
+    }
+}
