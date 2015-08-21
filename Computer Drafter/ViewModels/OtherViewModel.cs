bf1691ae6a6fdea32a5fdfb954d00a91cb170a2b
VesnaBrucoms <etstevens60@gmail.com>
2015-08-21 18:40:29 +0100

    Finished Other view.

---
+++ Computer Drafter/ViewModels/OtherViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Computer_Drafter.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer_Drafter.ViewModels
+{
+    class OtherViewModel : INotifyPropertyChanged
+    {
+        private OsModel osModel;
+
+        #region ModelProperties
+        public string OsName
+        {
+            get { return osModel.Name; }
+            set
+            {
+                osModel.Name = value;
+                OnPropertyChanged("OsName");
+            }
+        }
+
+        public decimal OsPrice
+        {
+            get { return osModel.Price; }
+            set
+            {
+                osModel.Price = value;
+                OnPropertyChanged("OsPrice");
+            }
+        }
+        #endregion
+
+        public OtherViewModel()
+        {
+            osModel = new OsModel();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+                Console.WriteLine(propertyName);
+            }
+        }
+    }
+}
